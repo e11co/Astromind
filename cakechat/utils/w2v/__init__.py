3a5efe697d2f5b95283f017ea5e924e2bc47320a
unknown <Xianggod233@gmail.com>
2018-08-31 22:22:04 +0800

    Empty messy file directory

--- cakechat/utils/w2v/__init__.py
+++
@@ -1,2 +0,0 @@
-from cakechat.utils.w2v.model import get_w2v_model
-from cakechat.utils.w2v.utils import get_w2v_model_path, get_w2v_params_str
--- trained_model/contract/loginAndRegisterReadMe.md
+++
@@ -1,8 +0,0 @@
-### 
-
-registerloginbase.sol is accesscontrol.sol
-
-register.sol is registerloginbase.sol
-
-login.sol is register.sol
-
